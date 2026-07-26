0147196d1ec132fb0916901b0c8b2a4b7cc175cb
OnlyChallgener <710646773@qq.com>
2026-07-26 18:58:46 +0800

    feat: install router task manager and bump Hub 0.9.28

--- hub_entry.py
+++ hub_entry.py
@@ -21,9 +21,10 @@ from router_realtime_stability_patch import (
 from router_relay_credentials_patch import install_router_relay_credentials_patch
 from router_rpc_v010 import create_router_blueprint_v010
 from router_slow_cache_patch import install_router_slow_cache_patch
+from router_task_manager_patch import install_router_task_manager_patch
 from router_ws_patch import install_router_ws_patch
 
-HUB_VERSION = "0.9.27"
+HUB_VERSION = "0.9.28"
 hub.APP_VERSION = HUB_VERSION
 install_router_http_developer_transport_patch()
 install_router_developer_flow_patch()
@@ -38,6 +39,7 @@ install_router_relay_credentials_patch()
 install_router_slow_cache_patch()
 install_router_control_scheduler_patch()
 install_router_control_actor_patch()
+install_router_task_manager_patch(hub)
 hub.app.register_blueprint(
     create_router_blueprint_v010(
         check_app_token=hub.check_app_token,
