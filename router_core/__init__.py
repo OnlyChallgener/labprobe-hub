120dcbd0d7b3f37b5a906c86e3bcd5fee756e0d5
OnlyChallgener <710646773@qq.com>
2026-08-24 16:42:47 +0800

    refactor: add router core contracts and interfaces

---
+++ router_core/__init__.py
@@ -0,0 +1,60 @@
+"""Router Core v1 Package.
+
+Formal architectural boundary for Router management in LabProbe Hub.
+"""
+
+from .contracts import (
+    RouterCapabilities,
+    RouterStatus,
+    NativePortMapRule,
+    UpnpRule,
+    UpnpState,
+    FirewallRule,
+    FirewallState,
+    DdnsRecord,
+    DdnsState,
+    Ipv6WanConfig,
+    Ipv6LanConfig,
+    Ipv6Config,
+    Ipv6Status,
+    Dhcpv6Client,
+    RouterDiagnosticItem,
+    RouterDiagnostic,
+)
+from .errors import (
+    RouterCoreError,
+    RouterNotConfiguredError,
+    RouterUnreachableError,
+    RouterAuthError,
+    RouterAuthExpiredError,
+    RouterFeatureDisabledError,
+    RouterRpcExecutionError,
+    RouterValidationError,
+)
+
+__all__ = [
+    "RouterCapabilities",
+    "RouterStatus",
+    "NativePortMapRule",
+    "UpnpRule",
+    "UpnpState",
+    "FirewallRule",
+    "FirewallState",
+    "DdnsRecord",
+    "DdnsState",
+    "Ipv6WanConfig",
+    "Ipv6LanConfig",
+    "Ipv6Config",
+    "Ipv6Status",
+    "Dhcpv6Client",
+    "RouterDiagnosticItem",
+    "RouterDiagnostic",
+    "RouterCoreError",
+    "RouterNotConfiguredError",
+    "RouterUnreachableError",
+    "RouterAuthError",
+    "RouterAuthExpiredError",
+    "RouterFeatureDisabledError",
+    "RouterRpcExecutionError",
+    "RouterValidationError",
+]
---
+++ router_core/driver/__init__.py
@@ -0,0 +1,3 @@
+from .base import RouterDriver
+
+__all__ = ["RouterDriver"]
